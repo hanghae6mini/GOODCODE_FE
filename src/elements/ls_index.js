9863b1263f0f947575c1d4565a0606e1df23039b
douchebag1108 <93530462+douchebag1108@users.noreply.github.com>
2022-04-13 19:14:14 +0900

    Update ls_index.js

--- src/elements/ls_index.js
+++ src/elements/ls_index.js
@@ -1,6 +1,6 @@
-import Grid from "./ls_Grid";
-import Text from "./ls_Text";
-import Input from "./ls_input";
-import Button from "./ls_Button";
+import ls_Grid from "./ls_Grid";
+import ls_Text from "./ls_Text";
+import ls_input from "./ls_input";
+import ls_Button from "./ls_Button";
 
-export {Grid, Text, Input, Button};
+export {ls_Grid, ls_Text, ls_input, ls_Button};
