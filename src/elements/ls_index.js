01ad45c3b0da5f7ab56a9998dfadbc5c66fea958
douchebag1108 <93530462+douchebag1108@users.noreply.github.com>
2022-04-13 23:18:43 +0900

    Delete ls_index.js

--- src/elements/ls_index.js
+++
@@ -1,6 +0,0 @@
-import ls_Grid from "./ls_Grid";
-import ls_Text from "./ls_Text";
-import ls_input from "./ls_input";
-import ls_Button from "./ls_Button";
-
-export {ls_Grid, ls_Text, ls_input, ls_Button};
